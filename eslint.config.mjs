6ec3b0ef20ddc28b82afba61c1867ff2f232546e
kazuki4real-hotech <kazuki.kitada@hotech.jp>
2026-07-14 22:20:08 +0900

    Add validation and CI workflow

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTypeScript from "eslint-config-next/typescript";
+
+export default defineConfig([
+  ...nextVitals,
+  ...nextTypeScript,
+  {
+    rules: {
+      "@next/next/google-font-display": "off",
+      "@next/next/no-html-link-for-pages": "off",
+      "@next/next/no-page-custom-font": "off",
+    },
+  },
+  globalIgnores([
+    "**/.next/**",
+    "**/.open-next/**",
+    "**/cloudflare-env.d.ts",
+    "prototype/**",
+    "node_modules/**",
+  ]),
+]);
